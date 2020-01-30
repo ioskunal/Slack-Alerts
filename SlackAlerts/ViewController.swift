a69730e79696e4fb50555ca5a20e8f04c4a19c0c
Kunal Gupta <ioskunal@gmail.com>
2020-01-30 13:11:06 +0530

    Initial Commit

---
+++ SlackAlerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlackAlerts
+//
+//  Created by Kunal Gupta on 30/01/20.
+//  Copyright © 2020 Kunal Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
