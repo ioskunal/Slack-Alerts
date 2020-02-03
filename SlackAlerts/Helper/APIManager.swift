d907e0892eb15aaeff57b84f0561c4216d467111
Kunal Gupta <ioskunal@gmail.com>
2020-02-03 23:40:33 +0530

    API Manager added.

---
+++ SlackAlerts/Helper/APIManager.swift
@@ -0,0 +1,13 @@
+//
+//  APIManager.swift
+//  SlackAlerts
+//
+//  Created by Kunal Gupta on 03/02/20.
+//  Copyright © 2020 Kunal Gupta. All rights reserved.
+//
+
+import Foundation
+
+struct APIManager {
+
+}
