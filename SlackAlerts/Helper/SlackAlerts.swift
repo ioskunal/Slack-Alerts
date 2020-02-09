1bedb71f3aa7830489ed59b40c13dda4b9a9f44a
Kunal Gupta <ioskunal@gmail.com>
2020-02-09 23:57:24 +0530

    - App Version, Build Number, Platform , Error added to Slack Alerts - Dynamic field "short" added

--- SlackAlerts/Helper/SlackAlerts.swift
+++ SlackAlerts/Helper/SlackAlerts.swift
@@ -15,20 +15,25 @@ struct SlackAlerts {
     static private var slackWebhook = "SLACK_WEBHOOK_URL"
 
     internal static func postToSlack(_ message: String) {
+        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+        let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? ""
+
         var arrFields = [JSON]()
-        arrFields.append(getField("App Version", "1.0"))
-        arrFields.append(getField("Build Number", "1"))
+        arrFields.append(getField(title: "App Version", message: appVersion, isShort: true))
+        arrFields.append(getField(title: "Build Number", message: buildNumber, isShort: true))
+        arrFields.append(getField(title: "Platform", message: "iOS", isShort: true))
+        arrFields.append(getField(title: "Error", message: "Data could not be read.", isShort: false))
         let dictAttachment = ["title": message, "fields": arrFields] as [String : Any]
         var arrAttachments = [JSON]()
         arrAttachments.append(dictAttachment)
         APIManager.postToSlack(slackWebhook, arrAttachments)
     }
     
-    private static func getField(_ title: String, _ message: String) -> JSON {
+    private static func getField(title: String, message: String, isShort: Bool = true) -> JSON {
         var dict = JSON()
         dict["title"] = title
         dict["value"] = message
-        dict["short"] = true
+        dict["short"] = isShort
         return dict
     }
 
